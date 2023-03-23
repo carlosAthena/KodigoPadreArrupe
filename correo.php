287f57d0d1d641a921d4980f58a384ff489b049a
Carlos Mancía <119626823+carlosAthena@users.noreply.github.com>
2023-03-22 18:46:05 -0600

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,35 @@
+<?php
+
+if (isset($_POST['enviar'])) {
+  if (!empty($_POST['nombre'])
+  && !empty($_POST['dui'])
+  && !empty($_POST['carnet'])
+  && !empty($_POST['email']) 
+  && !empty($_POST['date']) 
+  && !empty($_POST['hora'])
+  && !empty($_POST['rememberMe'])) {
+  
+    $nombre = $_POST['nombre'];
+    $dui = $_POST['dui'];
+    $carnet = $_POST['carnet'];
+    $email = $_POST['email'];
+    $fecha = $_POST['date'];
+    $hora= $_POST['hora'];
+    $reservacion = "Reserva el día " . $date . " a las " . $hora;
+    $datos = " Mi nombre es " . $nombre . ", mi dui es: " . $dui . ", mi carnet es: " . $carnet . ", y mi correo es: " . $email;
+
+$to = "carlosdavidmancia@gmail.com";
+
+$headers = "From: " . $email  . "\r\n" .
+"CC: charlycodejsx@gmail.com";
+
+
+
+    if ($mail!=NULL) {
+      mail($to, $datos, $reservacion, $headers);
+    }
+    header('Location:gracias.html');
+}
+}
+
+?>
